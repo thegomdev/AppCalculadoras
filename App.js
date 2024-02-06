196053a58173f1e77bdaf032d6354b941738739d
MARCELO GOMES JUNIOR <marcelo_gomes-jun1@estudante.sesisenai.org.br>
2024-02-06 14:22:07 -0300

    Criando as rotas

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+
+// Home
+import Home from './src/screens/Home';
+
+// Calculadora
+import Calculadora from './src/screens/Calculadora';
+
+// Calculadora Juros Compostos
+import JurosCompostos from './src/screens/JurosCompostos';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+        {/* Tela Home */}
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Tela inicial',
+            headerShown: false,
+          }}
+        />
+
+
+        {/* Tela Calculadora */}
+        <Stack.Screen
+          name="Calculadora"
+          component={Calculadora}
+          options={{
+            title: 'Tela inicial',
+            headerShown: false,
+          }}
+        />
+
+
+        {/* Tela Calculadora Juros Compostos */}
+        <Stack.Screen
+          name="JurosCompostos"
+          component={JurosCompostos}
+          options={{
+            title: 'Tela inicial',
+            headerShown: false,
+          }}
+        />
+
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
